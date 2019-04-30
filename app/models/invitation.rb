57e91cf2f7ca3dc0d943532e3b9265debffd5885
jonmackeyparty <jon@jonmackey.party>
2019-04-30 16:19:52 -0400

    add relationships to League and Invitation classes

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,2 +1,4 @@
 class Invitation < ApplicationRecord
+  belongs_to :league
+  belongs_to :player
 end
--- app/models/league.rb
+++ app/models/league.rb
@@ -1,2 +1,6 @@
 class League < ApplicationRecord
+  belongs_to :admin
+  has_many :teams
+  has_many :players, through: :invitations
+  has_many :invitations
 end
