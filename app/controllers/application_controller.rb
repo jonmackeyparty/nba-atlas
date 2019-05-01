25dfe15d05a16c67b28e562f6a5d631677eb26f5
jonmackeyparty <jon@jonmackey.party>
2019-05-01 18:32:47 -0400

    refine session create, login form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,11 @@
 class ApplicationController < ActionController::Base
 
   def current_user
-    @player = Player.find(session[:id])
+    @player = Player.find(session[:player_id])
   end
-  
+
+  def logged_in?
+    session[:player_id] != nil && session[:player_id] != ""
+  end
+
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,25 +1,29 @@
 class SessionsController < ApplicationController
 
   def new
+    @player = Player.new
   end
 
   def create
+    #raise params.inspect
     @player = Player.find_by(name: params[:name])
-    return head(:forbidden) unless @player.authenticate(params[:password])
-    session[:player_id] = @player.id
+    if @player
+      return head(:forbidden) unless @player.authenticate(params[:password])
+      session[:player_id] = @player.id
+      redirect_to player_path(@player)
+    else
+      redirect_to '/login'
+      #flash message goes here
+    end
   end
 
   def destroy
     if logged_in?
-      session.delete :name
+      session.delete :player_id
       render 'sessions/new'
     else
       redirect_to '/login'
     end
-  end
-
-  def logged_in?
-    session[:name] != nil && session[:name] != ""
   end
 
 end
