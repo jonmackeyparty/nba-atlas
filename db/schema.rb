26174a3ce85825d396c3ccb7bc3c56ecde887a01
jonmackeyparty <jon@jonmackey.party>
2019-05-01 10:11:06 -0400

    add password_digest to players table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_30_200408) do
+ActiveRecord::Schema.define(version: 2019_05_01_140947) do
 
   create_table "admins", force: :cascade do |t|
     t.string "name"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2019_04_30_200408) do
     t.integer "jersey_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
   create_table "teams", force: :cascade do |t|
