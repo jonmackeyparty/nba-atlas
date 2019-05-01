373da1dc68485c80678ab4fc4241cfac8b421ad4
jonmackeyparty <jon@jonmackey.party>
2019-05-01 10:45:41 -0400

    add player create helper method

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -5,10 +5,21 @@ class PlayersController < ApplicationController
   end
 
   def create
+    @player = Player.new(player_params)
+    if @player.checks_out?
+      raise params.inspect
+    else
+      render '/signup'
+    end 
   end
 
   def destroy
   end
 
+  private
+
+  def player_params
+    params.require(:player).permit(:name, :age, :nickname, :position, :jersey_number, :password, :password_confirmation)
+  end
 
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -2,4 +2,9 @@ class Player < ApplicationRecord
   has_many :teams, through: :leagues
   has_many :leagues, through: :invitations
   has_many :invitations
+
+  def checks_out?
+    self.password == self.password_confirmation && Player.find_by(:name => self.name) == nil
+  end
+  
 end
