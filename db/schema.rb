04becd549987876396878e47d38427e07e23ec18
jonmackeyparty <jon@jonmackey.party>
2019-05-01 10:17:55 -0400

    add password_confirmation to players table, stub out players/new form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_01_140947) do
+ActiveRecord::Schema.define(version: 2019_05_01_141647) do
 
   create_table "admins", force: :cascade do |t|
     t.string "name"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2019_05_01_140947) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "password_confirmation"
   end
 
   create_table "teams", force: :cascade do |t|
