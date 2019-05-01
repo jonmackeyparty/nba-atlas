33ef6d08c0137352ceee529a3dc1a38c8bcecd4b
jonmackeyparty <jon@jonmackey.party>
2019-05-01 17:10:59 -0400

    add admin_show partial, stub out sessions controller actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,27 @@ class SessionsController < ApplicationController
   end
 
   def create
+      session[:name] = params[:name]
+    if !logged_in?
+      redirect_to '/login'
+    else
+      redirect_to '/'
+    end
   end
 
   def destroy
-  end 
+    def destroy
+    if logged_in?
+      session.delete :name
+      render 'sessions/new'
+    else
+      redirect_to '/login'
+    end
+  end
+  end
 
+  def logged_in?
+    session[:name] != nil && session[:name] != ""
+  end
 
 end
