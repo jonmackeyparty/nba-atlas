bf624edf12d0b1db0abd23794c9e87ade7df1e2c
jonmackeyparty <jon@jonmackey.party>
2019-05-02 14:34:41 -0400

    add recent scope to invitations, permit recently issued invitations to show on player show page

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -2,12 +2,18 @@ class Invitation < ApplicationRecord
   belongs_to :league
   belongs_to :player
   scope :pending, -> { where(accepted: false) }
+  scope :approved, -> { where(accepted: true) }
+  scope :recent, -> { where(created_at: 1.week.ago..DateTime.now) }
 
   def league_name
     League.find(self.league_id).name
   end
 
   def admin_name
+    League.find(self.league_id).player.name
+  end
+
+  def player_name
     Player.find(self.player_id).name
   end
 
