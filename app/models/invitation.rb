f718da4601b552f5f4a8ba077c1bb2cd9ed4e468
jonmackeyparty <jon@jonmackey.party>
2019-05-02 13:29:00 -0400

    add invitation helper methods

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -6,5 +6,13 @@ class Invitation < ApplicationRecord
   def league_name
     League.find(self.league_id).name
   end
+
+  def admin_name
+    Player.find(self.player_id).name
+  end
+
+  def created_date
+    self.created_at.strftime("%A, %b %d")
+  end
   
 end
